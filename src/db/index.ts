e211a94e4e8b48d7bb82b800b2b0728c8c9e4a7d
Bryan <brn.dbn@gmail.com>
2026-01-30 15:29:16 -0600

    chore(prisma): make PrismaClient import runtime-compatible; fix build & lint scripts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg as any
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const dbUrl = process.env.DATABASE_URL
